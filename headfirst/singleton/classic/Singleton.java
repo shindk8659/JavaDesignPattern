960fa983dc5c00522a6a0776ce80f59eba6669ac
shindongkyu <shindk8659@naver.com>
2015-10-31 18:53:08 +0900

    singleton

---
+++ headfirst/singleton/classic/Singleton.java
@@ -0,0 +1,15 @@
+package headfirst.singleton.classic;
+
+public class Singleton {
+ private static Singleton uniqueInstance;
+    private static int numCalled=0;
+ private Singleton() {}
+ 
+ public static Singleton getInstance() {
+  if (uniqueInstance == null) {
+   uniqueInstance = new Singleton();
+  }
+                System.out.println("numCalled: "+numCalled++);
+  return uniqueInstance;
+ }
+}
