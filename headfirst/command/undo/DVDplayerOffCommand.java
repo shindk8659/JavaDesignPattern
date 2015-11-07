73d0d0a1aa60a236d5c1ec8195fecfa9e1d1ea55
shindongkyu <shindk8659@naver.com>
2015-11-07 23:38:41 +0900

    command

---
+++ headfirst/command/undo/DVDplayerOffCommand.java
@@ -0,0 +1,22 @@
+package headfirst.command.undo; 
+ 
+ 
+ public class DVDplayerOffCommand implements Command { 
+  DVDplayer dvd; 
+ int volumn; 
+   
+  public DVDplayerOffCommand(DVDplayer dvd){ 
+      this.dvd=dvd; 
+   } 
+   
+  public void undo() { 
+   dvd.volumeLevel(volumn); 
+ 
+  } 
+ 
+  public void execute(){ 
+  volumn=dvd.getvolumnLevel(); 
+     dvd.off();  
+    
+  } 
+ } 
--- headfirst/command/undo/RemoteLoader.java
+++ headfirst/command/undo/RemoteLoader.java
@@ -6,18 +6,36 @@ public class RemoteLoader {
   RemoteControlWithUndo remoteControl = new RemoteControlWithUndo();
  
   Light livingRoomLight = new Light("Living Room");
+  DVDplayer dvdplayer = new DVDplayer("Living Room");
 
   LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+  DVDplayerOnCommand dvdplayerOn = new DVDplayerOnCommand(dvdplayer);
+   
   LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+  DVDplayerOffCommand dvdplayerOff = new DVDplayerOffCommand(dvdplayer);
 
   remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
- 
+   remoteControl.setCommand(1, dvdplayerOn, dvdplayerOff);
+   
+   
+  System.out.println(remoteControl);
   remoteControl.onButtonWasPushed(0);
   remoteControl.offButtonWasPushed(0);
   System.out.println(remoteControl);
   remoteControl.undoButtonWasPushed();
   remoteControl.offButtonWasPushed(0);
   remoteControl.onButtonWasPushed(0);
+  System.out.println(remoteControl);
+  remoteControl.undoButtonWasPushed();
+  
+  
+  System.out.println(remoteControl);
+  remoteControl.onButtonWasPushed(1);
+  remoteControl.offButtonWasPushed(1);
+  System.out.println(remoteControl);
+  remoteControl.undoButtonWasPushed();
+  remoteControl.offButtonWasPushed(1);
+  remoteControl.onButtonWasPushed(1);
   System.out.println(remoteControl);
   remoteControl.undoButtonWasPushed();
  }
