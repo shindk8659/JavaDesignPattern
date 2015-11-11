34d492198688427f7712ab72d56fb58dbd98002a
LunaticSeeker <016jih@gmail.com>
2015-11-11 13:06:54 +0900

    'adapter'

---
+++ headfirst/adapter/duck/Duck.java
@@ -0,0 +1,6 @@
+package headfirst.adapter.duck;
+public  interface Duck{
+   public void quack();
+   public void fly();
+  
+}
---
+++ headfirst/adapter/duck/WildTurkey.java
@@ -0,0 +1,15 @@
+package headfirst.adapter.duck;
+
+public class WildTurkey implements Turkey
+{
+  public void gobble()
+  {
+    System.out .println("Turkey gobble");
+  }
+  
+  public void fly()
+  {
+    System.out.println("I belive i can fly");
+  }
+  
+}
