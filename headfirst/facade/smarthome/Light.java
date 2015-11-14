77b98feb2d9547b87dcef35273e46746e5774de0
shindk8659 <shindk8659@naver.com>
2015-11-14 18:24:47 +0900

    smart home

---
+++ headfirst/facade/smarthome/Light.java
@@ -0,0 +1,26 @@
+package headfirst.facade.smarthome;
+ 
+ 
+ public class Light { 
+  String description; 
+   
+  public Light(String description) { 
+   this.description = description; 
+  } 
+    
+  public void on() { 
+   System.out.println(description + " on"); 
+  } 
+    
+  public void off() { 
+   System.out.println(description + " off"); 
+  } 
+    
+  public void dim(int level) { 
+   System.out.println(description + " brightness  to " + level  + "%"); 
+  } 
+     
+         public String toString() { 
+                 return description; 
+         } 
+ } 
---
+++ headfirst/facade/smarthome/Screen.java
@@ -0,0 +1,20 @@
+package headfirst.facade.smarthome;
+
+public class Screen { 
+  String description; 
+   
+  public Screen(String description) { 
+   this.description = description; 
+  } 
+   
+  public void up() { 
+   System.out.println(description + " up"); 
+  } 
+   
+  public void down() { 
+   System.out.println(description + " down"); 
+  } 
+         public String toString() { 
+                 return description; 
+         } 
+ } 
